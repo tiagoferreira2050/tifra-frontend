4ebdd6ff2cc9d49ea9d60257f8a1d61825916cbd
Tiago Ferreira <tiagoferreira2050@hotmail.com>
2025-12-03 20:50:09 -0300

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -4,26 +4,27 @@ export function middleware(req: Request) {
   const url = new URL(req.url);
   const host = req.headers.get("host") || "";
 
-  // REMOVE porta quando local (ex: localhost:3000)
   const cleanHost = host.split(":")[0];
-
-  // Seu domínio principal
   const mainDomain = "tifra.com.br";
 
-  // Se for o domínio principal, segue normalmente
+  // Ignorar painel
+  if (cleanHost.startsWith("app.")) {
+    return NextResponse.next();
+  }
+
+  // Ignorar domínio principal
   if (cleanHost === mainDomain) {
     return NextResponse.next();
   }
 
-  // Pega o subdomínio
+  // Obter subdomínio
   const subdomain = cleanHost.replace(`.${mainDomain}`, "");
 
-  // Proteção
   if (!subdomain || subdomain === "www") {
     return NextResponse.next();
   }
 
-  // Reescreve a URL internamente para a rota com slug
+  // Redirecionar subcado para /store/[slug]
   url.pathname = `/store/${subdomain}`;
 
   return NextResponse.rewrite(url);
