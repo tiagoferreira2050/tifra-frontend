b5d6309dc5478d4a63d78391b10281dd40e62a98
Tiago Ferreira <tiagoferreira2050@hotmail.com>
2025-12-04 13:42:10 -0300

    fix: middleware matcher ignore assets

--- middleware.ts
+++ middleware.ts
@@ -39,6 +39,7 @@ export function middleware(req: NextRequest) {
 
 export const config = {
   matcher: [
-    "/:path*"
-  ],
+    '/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)'
+  ]
 };
+
